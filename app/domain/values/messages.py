1214e83c73e5ca6c713950dc3abdfe49f57c954d
vegetablepage <dany.titoff2016@yandex.ru>
2024-11-24 23:34:18 +0500

    first commit

---
+++ app/domain/values/messages.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from domain.exceptions.message import TextTooLongException
+from domain.values.base import BaseValueObject
+
+
+@dataclass(frozen=True)
+class Text(BaseValueObject):
+    value: str
+    def validate(self):
+        if len(self.value) > 255:
+            raise TextTooLongException(self.value)
+
+    def as_generic_type(self):
+        return str(self.value)
